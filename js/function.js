3d313ebcef91aad3e04d721567babe851b5dd70b
Marcelo Schloymann <marceloschloymann@gmail.com>
2023-01-19 08:14:59 -0300

    realice vinculación de js a html, hice algunos eventos de clik y mouse y algunos cambios con css

---
+++ js/function.js
@@ -0,0 +1,24 @@
+
+const button = document.getElementById ('button')
+
+
+
+button.addEventListener('click', () =>{
+    console.log('Enviado');
+})
+
+document.getElementById('nav').addEventListener('click', function(){
+    console.log('Clik')
+});
+
+document.getElementById('redes').addEventListener('click', function(){
+    console.log('Clik')
+});
+
+document.getElementById('perfil').addEventListener('mouseenter', function() {
+    perfil.style.background = "gray"
+} );
+
+document.getElementById('perfil').addEventListener('mouseleave', function() {
+    perfil.style.background = "white"
+} );
